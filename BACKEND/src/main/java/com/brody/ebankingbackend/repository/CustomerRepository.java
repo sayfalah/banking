ae02454fe1d5b82b666df4b6c084e8526d5bf3c9
malekalghraba <malekalghraba.gmail.com>
2023-08-25 13:41:01 +0100

    first commit

---
+++ BACKEND/src/main/java/com/brody/ebankingbackend/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.brody.ebankingbackend.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import com.brody.ebankingbackend.entities.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	
+	@Query("select c from Customer c where c.name like :kw")
+	List<Customer> findByNameContains(@Param("kw") String keywords);
+}
